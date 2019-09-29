7d3c2eec09f046bf71679ed2148a81d36078fa1d
zhoutao_yq <zhoutao_yq@kedacom.com>
2019-09-29 17:39:39 +0800

    update

--- src/main/java/com/leyou/demo/SmsDemo.java
+++ src/main/java/com/leyou/demo/SmsDemo.java
@@ -38,8 +38,8 @@ public class SmsDemo {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "";
-    static final String accessKeySecret = "";
+    static final String accessKeyId = "LTAI4FpofCDXTVBgJAhRWMeQ";
+    static final String accessKeySecret = "PyOUQrhx5tuRXhmxRi7ZC2fYXZlzO9";
 
     public static SendSmsResponse sendSms() throws ClientException {
 
@@ -58,7 +58,7 @@ public class SmsDemo {
         //必填:待发送手机号
         request.setPhoneNumbers("18852978753");
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("乐多佳商城");
+        request.setSignName("");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode("SMS_174992110");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
